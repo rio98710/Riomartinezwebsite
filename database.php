63648cc1817a977b10aa480ea7fdcfb036408d7c
rio98710 <analizavasquez842@gmail.com>
2025-03-01 07:12:47 +0800

    Add files via upload

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+$con=new
+mysqli("0.0.0.0","root","rio","newdb");
+$name=$_POST['name'];
+$gender=$_POST['gender'];
+$sql="INSERT INTO
+
+namegender(name,gender)
+VALUES('$name','$gender')";
+$result=mysqli_query($con,$sql);
+if($result=="true"){
+echo"<center><h2>Submitted
+successfully..</h2></center>
+}
+else{
+echo"<center><h2>Not submitted..
+</h2></center>";
+}
+
+
+?>
+
+
+
+
+
+
